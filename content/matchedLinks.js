545eec3586a838d9a4ce328f4b481b4b2819b16f
Maxwell Corbin <maxwell.corbin@gmail.com>
2016-06-18 18:29:49 -0400

    created file

---
+++ content/matchedLinks.js
@@ -0,0 +1,19 @@
+//find links on  page
+
+//get html of links
+//strip tags
+//search for regex
+//return y if found
+
+// res: {
+// 	[
+// 		{
+// 			url: "myUrl",
+// 			CaptureGroups: 
+// 			[
+// 				"textCg1",
+// 				"textCg2"
+// 			]
+// 		}
+// 	]
+// }
