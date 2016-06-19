6dd70c25b600ebc729f96eb7888f070f89e58ef5
hbaughman <hbaughman741@gmail.com>
2016-06-19 02:49:30 -0400

    Move through active highlight with next/prev

--- content/highlightMatches.js
+++ content/highlightMatches.js
@@ -3,11 +3,19 @@ chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     // Where `isDeep` is true is handled in `content/matchedLinks.js`
     if (!request.fields.isDeep) {
-      if (request.message === "submit_search") {
-        highlightMatches(request.fields)
-      }
-      else if (request.message === "clear_search") {
-        clearHighlights()
+      switch(request.message) {
+        case "submit_search":
+          highlightMatches(request.fields)
+          break
+        case "clear_search":
+          clearHighlights()
+          break
+        case "next_highlight":
+          moveCurrentHighlight(1)
+          break
+        case "prev_highlight":
+          moveCurrentHighlight(-1)
+          break
       }
     }
   }
@@ -42,4 +50,24 @@ function clearHighlights() {
   $(".deepSearch-highlight").each(function() {
     $(this).replaceWith($(this).html())
   })
+}
+
+function moveCurrentHighlight(moveDirection) {
+  var oldIndex = $(".deepSearch-current-highlight")
+    .removeClass("deepSearch-current-highlight")
+    .attr('data-highlight-index')
+
+  var maxIndex = $(".deepSearch-highlight")
+    .last()
+    .attr('data-highlight-index')
+
+  var newIndex = Number(oldIndex) + moveDirection
+  if (newIndex < 0) {
+    newIndex = maxIndex
+  }
+  else if (newIndex > maxIndex) {
+    newIndex = 0
+  }
+  $(".deepSearch-highlight[data-highlight-index='" + newIndex + "']")
+    .addClass("deepSearch-current-highlight")
 }
