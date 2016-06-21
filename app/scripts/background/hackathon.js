4be8ba745000220db265101a9e085da94793b5c7
hbaughman <hbaughman741@gmail.com>
2016-06-21 19:41:13 -0400

    Refactor searchHTML

--- app/scripts/background/hackathon.js
+++ app/scripts/background/hackathon.js
@@ -3,17 +3,6 @@ import { stripHtml } from './searchUrl'
 
 export default setupListener;
 
-function getHTML(url, callback) {
-  $.ajax({
-    url: url,
-    datatype: "html",
-    success: callback,
-    error: function(err) {
-      console.error(url, err);
-    }
-  });
-}
-
 function setupListener() {
   chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
@@ -30,34 +19,32 @@ function setupListener() {
   );
 }
 
+function getHTML(url, callback) {
+  $.ajax({
+    url: url,
+    datatype: "html",
+    success: callback,
+    error: function(err) {
+      console.error(url, err);
+    }
+  });
+}
+
 function searchHTML(href, queryParams, sendResponse) {
   return function(response) {
-    var res = {
-      found: {}
-    }
-    // response = response.replace(/<\s*body[^>]*>([\s\S]*)<\s*\/\s*body\s*>/, "$1");
-    // response = response.replace(/<\s*script[^>]*>[\s\S]*<\s*\/\s*script\s*>/g, "");
-    // response = response.replace(/<\s*script[^>]*\/\s*>/g, "");
-    // response = response.replace(/<[^>]+>/g, "");
-    // response = response.replace(/(\r?\n){2,}/g, "\n");
-    
-    response = stripHtml(response)
+    const { isCaseInsensitive, isRegex, search } = queryParams
 
-    var regexFlags = queryParams.isCaseInsensitive ? "gi" : "g";
-    if (queryParams.isRegex) {
-      var regExp = new RegExp(queryParams.search, regexFlags);
-      res.found = response.match(regExp);
-    } 
-    else {
-      var plainSearch = new RegExp(regexEscape(queryParams.search), regexFlags);
-      res.found = response.match(plainSearch);
-    }
+    const text = stripHtml(response)
+    const regex = isRegex ? search : regexEscape(search)
+    const regexFlags = isCaseInsensitive ? "gi" : "g";
+
+    const found = text.match(new RegExp(regex, regexFlags))
 
     sendResponse()
     notifyContentOfMessage({ 
       message: "checked_url", 
       href: href,
-      matchesFound: res.found 
+      matchesFound: found
     })
   }
 }
