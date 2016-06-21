85f6b4f7c85c8975d1747b4d9f9274c6a24e0562
hbaughman <hbaughman741@gmail.com>
2016-06-21 13:33:20 -0400

    Add build step

---
+++ app/scripts/background.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Enable chromereload by uncommenting this line:
+// import './lib/livereload';
+
+chrome.runtime.onInstalled.addListener(function (details) {
+  console.log('previousVersion', details.previousVersion);
+});
+
+chrome.browserAction.setBadgeText({text: '\'Allo'});
+
+console.log('\'Allo \'Allo! Event Page for Browser Action');
---
+++ hackathon/README.md
@@ -0,0 +1,5 @@
+# DeepSearch
+
+Built at Angelhack Brooklyn 2016 by
+* Henry Baughman
+* Max Corbin
