abd7d4b73c559b10342a84381917631ce19af81d
hbaughman <hbaughman741@gmail.com>
2016-10-14 18:40:07 -0400

    Handle shallow searches for non-existant strings

--- app/scripts/content/shallow/scrollToElement.js
+++ app/scripts/content/shallow/scrollToElement.js
@@ -1,14 +1,16 @@
 import $ from "jquery"
 
 export default function scrollToElement(elements) {
-  const targetPosition = elements.first().offset().top
-  const viewportTop = $("body").scrollTop()
-  const viewportBottom = viewportTop + $(window).height()
+  if (elements.length) {
+    const targetPosition = elements.first().offset().top
+    const viewportTop = $("body").scrollTop()
+    const viewportBottom = viewportTop + $(window).height()
 
-  const isElementAboveViewport = viewportTop > targetPosition
-  const isElementBelowViewport = targetPosition > viewportBottom
-  // don't scroll if element is already in the viewport
-  if (isElementAboveViewport || isElementBelowViewport) {
-    window.scrollTo(0, targetPosition)
+    const isElementAboveViewport = viewportTop > targetPosition
+    const isElementBelowViewport = targetPosition > viewportBottom
+    // don't scroll if element is already in the viewport
+    if (isElementAboveViewport || isElementBelowViewport) {
+      window.scrollTo(0, targetPosition)
+    }
   }
 }
