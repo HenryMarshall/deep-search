44d7a406cfd73c441f876b31e188a6e564eccc7f
hbaughman <hbaughman741@gmail.com>
2016-10-13 21:52:28 -0400

    Attractive tooltips

--- app/scripts/content/deep/matches.js
+++ app/scripts/content/deep/matches.js
@@ -1,8 +1,9 @@
-import $ from 'jquery'
+const $ = require("jquery")
+require("tooltipster")
 
 export function initialize() {
   global.deepSearchMatches = {}
-  listenForMarkClick()
+  listenForHover()
 }
 
 export function saveMatch(href, matches) {
@@ -11,21 +12,39 @@ export function saveMatch(href, matches) {
   }
 }
 
-function listenForMarkClick() {
-  // TODO: rename to `.deep-search-match`
-  $("body").on('click', '.deepSearch-link-found', onLinkMarkClick)
-  // TODO: rename to `.deep-search-no-match`
-  // TODO: create `.deep-search-error`
-  $('body').on('click', '.deepSearch-link-not-found', e => e.preventDefault())
+function listenForHover() {
+  $("body").on(
+    "mouseenter",
+    ".deepSearch-link-found:not(.tooltipstered)",
+    handleMouseEnter
+  )
+
+  $('body').on("click", ".deepSearch-link-not-found", e => e.preventDefault())
+  // TODO: create `.deepSearch-error`
 }
 
-function onLinkMarkClick(event) {
+function handleMouseEnter(event) {
   event.preventDefault()
-  const href = $(this).parent().attr('href')
-  const matchString = global.deepSearchMatches[href]
-    .slice(0,10)
-    .map(match => match.replace(/\s/, ' '))
-    .join('\n')
 
-  alert(matchString)
+  const $this = $(this)
+  const href = $this.parent().attr('href')
+  const matches = global.deepSearchMatches[href]
+
+  let message = "<ul><li>"
+  message += matches
+    .slice(0,10)
+    .map(match =>
+      match
+        .replace(/\s/, ' ')
+        .replace("<", "&lt;")
+        .replace(">", "&gt;")
+    )
+    .join("</li><li>")
+  message += "</li></ul>"
+  if (matches.length > 10) {
+    message += `<p>Plus ${matches.length - 10} more</p>`
+  }
+
+  $this.tooltipster({ content: $(message) }).tooltipster("open")
 }
+
