93424cba171dc37dd161f50cf440439c0a219eae
hbaughman <hbaughman741@gmail.com>
2016-10-14 18:43:08 -0400

    Remove deep clearMarks logging

--- app/scripts/content/deep/clearMarks.js
+++ app/scripts/content/deep/clearMarks.js
@@ -2,7 +2,5 @@ import $ from 'jquery'
 
 export default function clearMarks() {
   $('.deepSearch-link-found, .deepSearch-link-not-found').remove()
-  console.log("global.deepSearchMatches: ", global.deepSearchMatches)
   global.deepSearchMatches = {}
-  console.log("global.deepSearchMatches: ", global.deepSearchMatches)
 }
