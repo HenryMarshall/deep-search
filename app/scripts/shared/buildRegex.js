dbd37f4e790b3fdac602bae04d0c664b31219bb3
hbaughman <hbaughman741@gmail.com>
2016-10-17 16:51:48 -0400

    Empty string throws RangeError not SyntaxError

--- app/scripts/shared/buildRegex.js
+++ app/scripts/shared/buildRegex.js
@@ -1,7 +1,7 @@
 export default function buildRegex(queryParams) {
   const { isCaseInsensitive, isRegex, search } = queryParams
   if (!search) {
-    throw new SyntaxError("Searching empty strings causes a memory explosion")
+    throw new RangeError("Searching empty strings causes a memory explosion")
   }
 
   const regex = isRegex ? search : regexEscape(search)
