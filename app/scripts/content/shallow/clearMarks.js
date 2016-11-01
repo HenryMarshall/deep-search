0248b373c3af58074d799c97035fa4583a941c6d
hbaughman <hbaughman741@gmail.com>
2016-11-01 17:38:29 -0400

    Cleanly restore text on clearing shallow marks
    When removing the highlight divisions were being left. If "ba" was
    searched in "foo bar baz" and then removed you were left with
    
        "ba"
        "r baz"
    
    When the next highlight occurs with the string "bar" each text node is
    surrounded thusly:
    
        <span>ba</span>
        <span>r</span>
        " baz"
    
    When cleared this leads to:
    
        "ba"
        "r"
        " baz"
    
    This pattern then continues to get worse in perpetuity.

--- app/scripts/content/shallow/clearMarks.js
+++ app/scripts/content/shallow/clearMarks.js
@@ -1,7 +1,24 @@
 import $ from 'jquery'
 
 export default function clearMarks() {
-  $(".deepSearch-highlight").each(function() {
-    $(this).replaceWith($(this).html())
+  $(".deepSearch-highlight").parent().each(function() {
+    const contents = []
+    const $parent = $(this)
+    $parent.contents().each(function() {
+      const $node = $(this)
+      let html
+
+      if ($node.hasClass("deepSearch-highlight")) {
+        html = $node.html()
+      }
+      else if (this.nodeName === "#text") {
+        html = this.data
+      }
+      else {
+        html = this.outerHTML
+      }
+      contents.push(html)
+    })
+    $parent.html(contents.join(""))
   })
 }
