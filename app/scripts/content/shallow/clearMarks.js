7598907429a132cbed8809418afc87ba9aa2b9e9
hbaughman <hbaughman741@gmail.com>
2016-12-16 12:55:41 -0500

    Cache on clearing shallow marks

--- app/scripts/content/shallow/clearMarks.js
+++ app/scripts/content/shallow/clearMarks.js
@@ -1,12 +1,11 @@
 import $ from 'jquery'
 
 export default function clearMarks() {
-  const $highlights = $(".deepSearch-highlight")
+  const $body = $("body")
+  const $bodyDouble = $("body").clone()
+  const $highlights = $bodyDouble.find(".deepSearch-highlight")
   const $parents = $highlights.parent()
 
-  // Performance may be improved if you perform this replacement inside the
-  // "$parents" that occurs later. This would reduce the number of times you
-  // touch the DOM -- potentially by a large margin.
   $highlights.replaceWith(function() {
     return $(this).contents()
   })
@@ -15,4 +14,6 @@ export default function clearMarks() {
     this.normalize()
     return this
   })
+
+  $body.replaceWith($bodyDouble)
 }
